e0a81694ee8bc4a9cc9876fa6491be38eb0043cf
dotPast <83116565+dotPast@users.noreply.github.com>
2024-01-07 14:33:18 +0300

    init: Bootstrap Project

---
+++ src/main/java/lsmpteam/virus/LSMPVirus.kt
@@ -0,0 +1,15 @@
+package lsmpteam.virus
+
+import org.bukkit.Bukkit
+import org.bukkit.plugin.java.JavaPlugin
+
+class LSMPVirus : JavaPlugin() {
+  override fun onEnable() {
+    // Plugin startup logic
+    Bukkit.getLogger().info("pemnis")
+  }
+
+  override fun onDisable() {
+    // Plugin shutdown logic
+  }
+}
